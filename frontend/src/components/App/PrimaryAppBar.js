f4d4827e597a447d18f85f880bd8391b11d493a4
Haoliang Zhang <hlzhang26@outlook.com>
2022-08-18 22:17:30 -0600

    add tooltips in primary appbar

--- frontend/src/components/App/PrimaryAppBar.js
+++ frontend/src/components/App/PrimaryAppBar.js
@@ -18,6 +18,7 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import MoreIcon from '@mui/icons-material/MoreVert';
+import Tooltip from '@mui/material/Tooltip';
 import withRouter from '../UI/withRouter';
 
 const drawerWidth = 240;
@@ -50,9 +51,6 @@ export default function PrimaryAppBar(props) {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  /*
-
-*/
   const { window } = props;
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
@@ -81,7 +79,7 @@ export default function PrimaryAppBar(props) {
     </Box>
   );
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = 'primary-account-menu';
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
@@ -144,7 +142,7 @@ export default function PrimaryAppBar(props) {
         <IconButton
           size="large"
           aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
+          aria-controls="primary-account-menu"
           aria-haspopup="true"
           color="inherit"
         >
@@ -179,35 +177,41 @@ export default function PrimaryAppBar(props) {
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="inherit"
-            >
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
+            <Tooltip title="Message">
+              <IconButton
+                size="large"
+                aria-label="show 4 new mails"
+                color="inherit"
+              >
+                <Badge badgeContent={4} color="error">
+                  <MailIcon />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Notifications">
+              <IconButton
+                size="large"
+                aria-label="show 17 new notifications"
+                color="inherit"
+              >
+                <Badge badgeContent={17} color="error">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Account">
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </Tooltip>
           </Box>
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
             <IconButton
